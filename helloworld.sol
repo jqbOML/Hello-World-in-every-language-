3fab00dedc309b00d1f13fa904c317acb6fce162
Jakub Omieljaniuk <56368562+jqbOML@users.noreply.github.com>
2020-05-04 21:17:30 +0200

    Update helloworld.sol

--- helloworld.sol
+++ helloworld.sol
@@ -1,7 +1,7 @@
-pragma solidity >=0.5.0 <0.6.0;
+pragma solidity ^0.6.0;
 
 contract HelloWorld {
-    function print() public pure returns(string memory) {
-        return "Hello World!";
-    }
+  function helloWorld() external pure returns (string memory) {
+    return "Hello, World!";
+  }
 }
